a739848cd8dcd9a1bb0d23fc81b7dcb74fef00d4
Reza Angkasa <72240159+reza7angkasa@users.noreply.github.com>
2021-02-24 21:15:09 +0700

    Add files via upload

---
+++ Materi Teks 3a dan 3b/Materi_3B_2.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+ import { Text } from 'react-native';
+
+ class Cat extends Component {
+ render() {
+ return (
+ <Text>Hello, I am your cat!</Text>
+ );
+ }
+}
+export default Cat; 
